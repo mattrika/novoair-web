a5b8542373006e575d34ad74d2399c34b39a5339
RafiulAlamRami <rafiul7368@gmail.com>
2025-01-09 11:27:17 +0600

    complete pages part 2

--- src/app/main/co-brandeed-prepaid-card/co-brandeed-card/co-brandeed-card.component.ts
+++ src/app/main/co-brandeed-prepaid-card/co-brandeed-card/co-brandeed-card.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'
 import { ButtonModule } from 'primeng/button'
 import { CardModule } from 'primeng/card'
 
 @Component({
     selector: 'app-co-brandeed-card',
-    imports: [ButtonModule, CardModule],
+    imports: [RouterModule,ButtonModule, CardModule],
     templateUrl: './co-brandeed-card.component.html',
     styleUrl: './co-brandeed-card.component.scss',
 })
--- src/app/main/smiles-frequent-flyer/smiles-card/smiles-card.component.ts
+++ src/app/main/smiles-frequent-flyer/smiles-card/smiles-card.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 @Component({
-  selector: 'app-smiles-card',
-  imports: [],
-  templateUrl: './smiles-card.component.html',
-  styleUrl: './smiles-card.component.scss'
+    selector: 'app-smiles-card',
+    imports: [RouterModule],
+    templateUrl: './smiles-card.component.html',
+    styleUrl: './smiles-card.component.scss',
 })
-export class SmilesCardComponent {
-
-}
+export class SmilesCardComponent {}
